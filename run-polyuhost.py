17d64fc91ec5aa0aac98c384e031383064860617
bianzheng123 <1317257555@qq.com>
2022-08-09 15:29:36 +0800

    add the run config

--- run-polyuhost.py
+++ run-polyuhost.py
@@ -191,11 +191,11 @@ if __name__ == '__main__':
     #     os.system(
     #         'cd build/attribution && ./pprnbi --dataset_name {}'.format(ds))
 
-    # os.system(
-    #     'cd build && ./brtt --dataset_name {}'.format('amazon'))
+    os.system(
+        'cd build && ./brtt --dataset_name {}'.format('amazon'))
     # os.system(
     #     'cd build && ./brqrbb --dataset_name {}'.format('amazon'))
-    os.system(
-        'cd build && ./brmrbi --dataset_name {}'.format('amazon'))
+    # os.system(
+    #     'cd build && ./brmrbi --dataset_name {}'.format('amazon'))
 
     # run_compress_topt()
