49fa6f231d0d082d8c3687efa1c5b32dfbfdbfc0
bianzheng123 <1317257555@qq.com>
2022-11-11 12:26:29 +0800

    add the run config

--- motivation/dimensionality_curse/run.py
+++ motivation/dimensionality_curse/run.py
@@ -5,15 +5,16 @@ from recbole.quick_start import run_recbole
 
 if __name__ == '__main__':
     model = 'ENMF'
-    dataset = 'ml-1m'
-    ebd_l = [1024]
+    dataset = ['lastfm', 'ml-1m']
+    ebd_l = [4, 8, 16, 32, 64, 128, 256]
     # ebd_l = [2]
     res_l = {}
     for ebd in ebd_l:
         # config_dict = {'embedding_size': 64, 'epochs': 100, 'neg_sampling': None}
         # config_dict = {'embedding_size': 64, 'epochs': 100, 'topk': [10], 'valid_metric': 'MRR@10',
         #                'metrics': ['Recall', 'NDCG', 'Hit', 'Precision']}
-        config_dict = {'embedding_size': ebd, 'epochs': 200, 'topk': [50, 100, 200], 'neg_sampling': None, 'use_gpu': False,
+        config_dict = {'embedding_size': ebd, 'epochs': 200, 'topk': [50, 100, 200], 'neg_sampling': None,
+                       'use_gpu': False,
                        'train_batch_size': 512, 'learning_rate': 0.05, 'weight_decay': 0.5, 'learner': 'adagrad',
                        'eval_step': 20,
                        'valid_metric': 'hit@200',
