ee3288958a807ab44cb14c200dbb7ed610fecd7a
bianzheng123 <1317257555@qq.com>
2022-12-01 22:47:00 +0800

    add the run config

--- test.cpp
+++ test.cpp
@@ -1,12 +1,12 @@
+#include <random>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <vector>
-#include <cstdlib>
 
-using namespace std;
-
-int main(int argc, char **argv) {
-    std::vector<int> v;
-    cout << v.max_size() << endl;
-    return 0;
+int main() {
+    std::seed_seq seq{1, 2, 3, 4, 5};
+    std::vector<std::uint32_t> seeds(5);
+    seq.generate(seeds.begin(), seeds.end());
+    for (std::uint32_t n: seeds) {
+        std::cout << n << '\n';
+    }
 }
