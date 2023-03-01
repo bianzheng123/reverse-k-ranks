d80396c76453634012e40d7722b0cd6da14d3ac7
bianzheng123 <1317257555@qq.com>
2023-03-01 17:05:30 +0800

    add the parameter of build score table

--- run_dbg_other.py
+++ run_dbg_other.py
@@ -5,6 +5,15 @@ from script.data_convert import vecs_io
 import run_polyuhost as polyu
 
 
+def test_build_score_table(dataset_name: str, eval_size_gb: int = 100):
+    username = 'bianzheng'
+    dataset_dir = f'/home/{username}/Dataset/ReverseMIPS'
+    index_dir = f'/home/{username}/reverse-k-ranks/index'
+    os.system(
+        f'cd build && ./bstb --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --eval_size_gb {eval_size_gb} ')
+    os.system(f'cd index && rm {dataset_name}.index')
+
+
 def run():
     # dataset_l = ['movielens-27m', 'netflix', 'yahoomusic_big', 'yelp', 'amazon-home-kitchen']
     dataset_l = ['movielens-27m', 'netflix', 'yahoomusic_big', 'yelp',
@@ -18,90 +27,6 @@ def run():
         f'cd build && ./rri --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} '
         f'--test_topk {"false"} --method_name {"Simpfer"} --simpfer_k_max {k_max} --stop_time {43200}')
 
-    # dataset_name = 'movielens-27m'
-    # os.system(
-    #     'cd build && ./rri --dataset_dir {} --dataset_name {} --index_dir {} --test_topk {} --method_name {} --stop_time {}'.format(
-    #         dataset_dir, dataset_name, index_dir, 'false', 'GridIndex', 9000))
-
-    # for method_name, dataset_name, n_sample in [('MinMaxLinearRegression', 'yahoomusic_big', 588),
-    #                                             ('UniformLinearRegression', 'yelp', 490)]:
-    #     n_sample_query = 5000
-    #     for sample_topk in [10, 50, 100, 150, 200, 300, 400, 500, 600]:
-    #         os.system(
-    #             f'cd index/memory_index && '
-    #             f'{method_name}-{dataset_name}-n_sample_{n_sample}-n_sample_query_{n_sample_query}-sample_topk_{sample_topk}.index '
-    #             f'{method_name}-{dataset_name}_more_query-n_sample_{n_sample}-n_sample_query_{n_sample_query}-sample_topk_{sample_topk}.index')
-    #         os.system(
-    #             f'cd index/memory_index && '
-    #             f'{method_name}-{dataset_name}-n_sample_{n_sample}-n_sample_query_{n_sample_query}-sample_topk_{sample_topk}.index '
-    #             f'QueryRankSampleSearchKthRank-{dataset_name}_more_query-n_sample_{n_sample}-n_sample_query_{n_sample_query}-sample_topk_{sample_topk}.index')
-    #
-    # dataset_name = "yahoomusic_big"  # movielens-27m yahoomusic_big yelp amazon-home-kitchen
-    # k_max = polyu.compute_k_max_in_reverse_mips(dataset_name, 0.5)
-    # os.system(
-    #     f"cd build && ./rtk --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} "
-    #     f"--test_topk {'false'} --method_name {'SimpferFEXIPROOnly'} --simpfer_k_max {k_max} --stop_time {70000} "
-    #     f"--test_reverse_topk {64}"
-    # )
-    #
-    # dataset_name = "yelp"  # movielens-27m yahoomusic_big yelp amazon-home-kitchen
-    # k_max = polyu.compute_k_max_in_reverse_mips(dataset_name, 0.5)
-    # os.system(
-    #     f"cd build && ./rtk --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} "
-    #     f"--test_topk {'false'} --method_name {'SimpferFEXIPROOnly'} --simpfer_k_max {k_max} --stop_time {70000} "
-    #     f"--test_reverse_topk {32}"
-    # )
-
-    # n_sample_item = 5000
-    # dataset_name = 'yelp'  # yelp, yahoomusic_big
-    # memory_capacity = 8
-    # method_name = "QueryRankSampleMinMaxIntLR"  # QueryRankSampleUniformIntLR QueryRankSampleMinMaxIntLR
-    # for sample_topk in [100, 200, 300, 400, 500]:
-    #     n_sample = polyu.compute_n_sample_by_memory_index_sample_only(dataset_name, memory_capacity)
-    #     os.system(
-    #         f"cd build && ./bilrbc --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
-    #     os.system(
-    #         f"cd index/memory_index && mv UniformLinearRegression-yahoomusic_big-n_sample_{n_sample}.index "
-    #         f"UniformLinearRegression-yahoomusic_big_n_sample_{n_sample}_n_sample_query_{n_sample_item}_sample_topk_{sample_topk}.index"
-    #     )
-    #
-    # sample_topk = 600
-    # dataset_name = 'yahoomusic_big'  # yelp, yahoomusic_big
-    # memory_capacity = 8
-    # method_name = "QueryRankSampleSearchUniformRankUniformIntLR"  # QueryRankSampleUniformIntLR QueryRankSampleMinMaxIntLR
-    # for n_sample_item in [5000]:
-    #     n_sample = polyu.compute_n_sample_by_memory_index_sample_only(dataset_name, memory_capacity)
-    #     os.system(
-    #         f"cd build && ./bilrbc --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
-    #
-    # dataset_name = 'yahoomusic_big'  # yelp, yahoomusic_big
-    # n_data_item = polyu.dataset_m[dataset_name][0]
-    # n_user = polyu.dataset_m[dataset_name][2]
-    # memory_capacity = 8
-    # method_name = "QueryRankSampleSearchKthRank"
-    # n_sample_item = 5000
-    # for sample_topk in [150]:
-    #     n_sample = polyu.compute_n_sample_by_memory_index_sample_only(dataset_name, memory_capacity)
-    #     os.system(
-    #         f"cd build && ./fsr --index_dir {index_dir} --dataset_name {dataset_name} --method_name {method_name} " +
-    #         f"--n_sample {n_sample} --n_data_item {n_data_item} --n_user {n_user} --n_sample_query {n_sample_item} --sample_topk {sample_topk}"
-    #     )
-    #     os.system(
-    #         f"cd build && ./bsibc --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} " +
-    #         f"--method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
-    #
-    # os.system('cd index/memory_index && '
-    #           'cp QueryRankSampleSearchKthRank-yahoomusic_big-n_sample_588-n_sample_query_5000-sample_topk_150.index'
-    #           'QueryRankSampleIntLR-yahoomusic_big-n_sample_588-n_sample_query_5000-sample_topk_150.index')
-    # n_sample_item = 5000
-    # dataset_name = 'yahoomusic_big'  # yelp, yahoomusic_big
-    # memory_capacity = 8
-    # method_name = "QueryRankSampleMinMaxIntLR"  # QueryRankSampleUniformIntLR QueryRankSampleMinMaxIntLR
-    # for sample_topk in [150]:
-    #     n_sample = polyu.compute_n_sample_by_memory_index_sample_only(dataset_name, memory_capacity)
-    #     os.system(
-    #         f"cd build && ./bilrbc --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
-
 
 if __name__ == '__main__':
     index_dir = os.path.join('/home', 'zhengbian', 'reverse-k-ranks', 'index')
@@ -110,4 +35,9 @@ if __name__ == '__main__':
     # dataset_l = ['movielens-27m', 'netflix', 'yahoomusic', 'yelp']
     # dataset_l = ['netflix-small', 'movielens-27m-small']
 
-    run()
+    # run()
+    test_build_score_table(dataset_name='movielens-27m', eval_size_gb=500)
+    test_build_score_table(dataset_name='netflix', eval_size_gb=500)
+    test_build_score_table(dataset_name='yahoomusic_big', eval_size_gb=500)
+    test_build_score_table(dataset_name='yelp', eval_size_gb=500)
+    test_build_score_table(dataset_name='amazon-home-kitchen', eval_size_gb=500)
--- src/app/BuildScoreTableBatch.cpp
+++ src/app/BuildScoreTableBatch.cpp
@@ -19,6 +19,7 @@
 class Parameter {
 public:
     std::string dataset_dir, dataset_name, index_dir;
+    uint64_t eval_size_gb;
 };
 
 void LoadOptions(int argc, char **argv, Parameter &para) {
@@ -34,7 +35,10 @@ void LoadOptions(int argc, char **argv, Parameter &para) {
              "dataset_name")
             ("index_dir, id",
              po::value<std::string>(&para.index_dir)->default_value("/home/bianzheng/reverse-k-ranks/index"),
-             "the directory of the index");
+             "the directory of the index")
+            ("eval_size_gb, esb",
+             po::value<uint64_t>(&para.eval_size_gb)->default_value(500),
+             "evaluation size, in terms of GB");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, opts), vm);
@@ -49,11 +53,32 @@ void LoadOptions(int argc, char **argv, Parameter &para) {
 using namespace std;
 using namespace ReverseMIPS;
 
+int ComputeEvalUserSize(const int &n_user, const int &n_data_item, const uint64_t &eval_size_gb) {
+    const uint64_t total_size_gb = (u_int64_t) n_user * n_data_item * sizeof(double) / 1024 / 1024 / 1024;
+    if (eval_size_gb > total_size_gb) {
+        return n_user;
+    }
+    const uint64_t n_eval_user = eval_size_gb * 1024 * 1024 * 1024 / sizeof(double) / n_data_item;
+    assert(n_eval_user < n_user);
+    return (int) n_eval_user;
+}
+
+int ComputeBatchUser(const int &n_user, const int &n_data_item, const int &vec_dim, const uint64_t &gpu_size_gb) {
+
+    const uint64_t dataset_size = sizeof(double) * (n_user + n_data_item) * vec_dim;
+    const uint64_t gpu_size = gpu_size_gb * 1000 * 1000 * 1000;
+    const uint64_t batch_n_user = (gpu_size - dataset_size) / (sizeof(double) + sizeof(int)) / n_data_item;
+    return (int) batch_n_user;
+}
+
 void BuildScoreTable(VectorMatrix &user, VectorMatrix &data_item,
-                     const char *index_path,
-                     double &total_compute_sort_time, double &total_save_index_time) {
+                     const char *index_path, const uint64_t &eval_size_gb,
+                     double &total_compute_sort_time, double &total_save_index_time,
+                     double &estimate_compute_sort_time, double &estimate_save_index_time) {
     const int n_user = user.n_vector_;
     const int n_data_item = data_item.n_vector_;
+    const int vec_dim = data_item.vec_dim_;
+    const int n_eval_user = ComputeEvalUserSize(n_user, n_data_item, eval_size_gb);
     //build index
     user.vectorNormalize();
     //disk index
@@ -66,12 +91,13 @@ void BuildScoreTable(VectorMatrix &user, VectorMatrix &data_item,
     double batch_compute_sort_time = 0;
     double batch_save_index_time = 0;
 
-    const uint64_t batch_n_user = 10;
+    const uint64_t batch_n_user = ComputeBatchUser(n_user, n_data_item, vec_dim, 11);
     //Compute Score Table
-    ComputeScoreTableBatch cstb(user, data_item, batch_n_user);
+    ComputeScoreTableBatch cstb(user, data_item, (int) batch_n_user);
 
     const int remainder = n_user % batch_n_user == 0 ? 0 : 1;
     const int n_batch = n_user / (int) batch_n_user + remainder;
+    spdlog::info("{} user per batch, n_batch {}", batch_n_user, n_batch);
     std::vector<DistancePair> batch_distance_l(batch_n_user * n_data_item);
 
     const uint32_t report_every = 30;
@@ -79,9 +105,13 @@ void BuildScoreTable(VectorMatrix &user, VectorMatrix &data_item,
     TimeRecord batch_record, operation_record;
     batch_record.reset();
 
+    int n_complete_user = 0;
     for (int batchID = 0; batchID < n_batch; batchID++) {
         operation_record.reset();
         const int start_userID = (int) batch_n_user * batchID;
+        if (start_userID > n_eval_user) {
+            break;
+        }
         const int n_user_batch = n_user - start_userID > batch_n_user ? (int) batch_n_user : n_user - start_userID;
         cstb.ComputeSortItemsBatch(start_userID, n_user_batch, batch_distance_l.data());
         const double tmp_compute_sort_time = operation_record.get_elapsed_time_second();
@@ -96,6 +126,8 @@ void BuildScoreTable(VectorMatrix &user, VectorMatrix &data_item,
         batch_compute_sort_time += tmp_compute_sort_time;
         batch_save_index_time += tmp_save_index_time;
 
+        n_complete_user += (int) batch_n_user;
+
         if (batchID % report_every == 0) {
             spdlog::info(
                     "preprocessed {:.1f}%, Mem: {} Mb, {} s/iter, compute sort time {}s, process index time {}s",
@@ -107,6 +139,8 @@ void BuildScoreTable(VectorMatrix &user, VectorMatrix &data_item,
             batch_record.reset();
         }
     }
+    estimate_compute_sort_time = total_compute_sort_time / n_complete_user * n_user;
+    estimate_save_index_time = total_save_index_time / n_complete_user * n_user;
     cstb.FinishCompute();
     disk_ins.FinishBuildIndex();
 }
@@ -120,6 +154,8 @@ int main(int argc, char **argv) {
     string index_dir = para.index_dir;
     spdlog::info("dataset_name {}, dataset_dir {}", dataset_name, dataset_dir);
     spdlog::info("index_dir {}", index_dir);
+    const uint64_t eval_size_gb = para.eval_size_gb;
+    spdlog::info("evaluation size {}GB", eval_size_gb);
 
     int n_data_item, n_query_item, n_user, vec_dim;
     vector<VectorMatrix> data = readData(dataset_dir, dataset_name, n_data_item, n_query_item, n_user,
@@ -134,13 +170,14 @@ int main(int argc, char **argv) {
 
     TimeRecord record;
     record.reset();
-    char parameter_name[256] = "";
 
     double total_compute_sort_time, total_save_index_time;
+    double estimate_compute_sort_time, estimate_save_index_time;
 
     BuildScoreTable(user, data_item,
-                    index_path,
-                    total_compute_sort_time, total_save_index_time);
+                    index_path, eval_size_gb,
+                    total_compute_sort_time, total_save_index_time,
+                    estimate_compute_sort_time, estimate_save_index_time);
 
     double build_index_time = record.get_elapsed_time_second();
     spdlog::info("finish preprocess and save the index");
@@ -149,13 +186,22 @@ int main(int argc, char **argv) {
 
     spdlog::info("build index time: total {}s, compute sort time {}s, save index time {}s",
                  build_index_time, total_compute_sort_time, total_save_index_time);
+    spdlog::info("estimate compute sort time {}s, estimate save index time {}s",
+                 estimate_compute_sort_time, estimate_save_index_time);
 
     char build_index_info[256];
-    sprintf(build_index_info, "compute sort time %.3f s, save index time %.3f s",
-            total_compute_sort_time, total_save_index_time);
+    sprintf(build_index_info,
+            "compute sort time %.3f s, save index time %.3f s, estimate compute sort time %.3f s, estimate save index time %.3f s",
+            total_compute_sort_time, total_save_index_time,
+            estimate_compute_sort_time, estimate_save_index_time);
 
     config.AddInfo(build_index_info);
     config.AddBuildIndexTime(build_index_time);
-    config.WritePerformance(dataset_name, "BuildIndex", parameter_name);
+
+#ifdef USE_GPU
+    config.WritePerformance(dataset_name, "BuildIndex", "GPU");
+#elif
+    config.WritePerformance(dataset_name, "BuildIndex", "CPU");
+#endif
     return 0;
 }
