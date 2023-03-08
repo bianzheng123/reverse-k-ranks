e7d08b32dc4d0dbdf8a7e7e2bb06adac7ab5894c
bianzheng123 <1317257555@qq.com>
2023-03-08 18:22:47 +0800

    add the run dbg disk

--- run_dbg_disk.py
+++ run_dbg_disk.py
@@ -11,8 +11,9 @@ def run():
     # dataset_l = ['amazon-home-kitchen']
     # dataset_l = ['netflix', 'movielens-27m']
 
+    index_dir = f'/home/{username}/reverse-k-ranks/index'
     for dataset_name in ['yahoomusic_big', 'yelp']:
-        for memory_capacity in [8, 16, 32, 64, 110]:
+        for memory_capacity in [32, 64, 110]:
             n_data_item = dbg.dataset_m[dataset_name][0]
             n_user = dbg.dataset_m[dataset_name][2]
             n_sample = dbg.compute_n_sample_by_memory_index_sample_only(dataset_name=dataset_name,
@@ -32,11 +33,29 @@ def run():
                 f"cd build && ./rri --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --test_topk {'false'} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}"
             )
 
-            method_name = 'RankSample'
+            method_name = 'QueryRankSampleComputeAll'
             os.system(
                 f"cd build && ./rri --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --test_topk {'false'} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}"
             )
-            method_name = 'RankSampleComputeAll'
+
+    index_dir = f'/home/{username}/reverse-k-ranks/index_amazon'
+    for dataset_name in ['amazon-home-kitchen']:
+        for memory_capacity in [32, 64, 110]:
+            n_data_item = dbg.dataset_m[dataset_name][0]
+            n_user = dbg.dataset_m[dataset_name][2]
+            n_sample = dbg.compute_n_sample_by_memory_index_sample_only(dataset_name=dataset_name,
+                                                                        memory_capacity=memory_capacity)
+            n_sample_item = 5000
+            sample_topk = 600
+            method_name = 'QueryRankSampleSearchKthRank'
+            os.system(
+                f"cd build && ./fsr --index_dir {index_dir} --dataset_name {dataset_name} --method_name {method_name} --n_sample {n_sample} --n_data_item {n_data_item} --n_user {n_user} --n_sample_query {n_sample_item} --sample_topk {sample_topk}"
+            )
+            os.system(
+                f"cd build && ./bsibs --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
+            # os.system(
+            #     f"cd build && ./bsibc --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}")
+
             os.system(
                 f"cd build && ./rri --dataset_dir {dataset_dir} --dataset_name {dataset_name} --index_dir {index_dir} --test_topk {'false'} --method_name {method_name} --n_sample {n_sample} --n_sample_query {n_sample_item} --sample_topk {sample_topk}"
             )
